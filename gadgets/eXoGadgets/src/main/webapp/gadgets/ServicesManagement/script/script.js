64d4265676fdcbc2512353fb95ef4abdb395e7b5
Tran The Trong <trongtt@gmail.com>
2012-02-23 09:52:57 +0700

    Use jquery from gatein resource controller for Service Management gadget

--- gadgets/eXoGadgets/src/main/webapp/gadgets/ServicesManagement/script/script.js
+++ gadgets/eXoGadgets/src/main/webapp/gadgets/ServicesManagement/script/script.js
@@ -16,9 +16,7 @@
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
  * site: http://www.fsf.org.
  */
-eXo = {
-	gadget : {}
-};
+var eXo = eXo || {gadget : {}};
 
 function ServicesManagement() {
 	this.DEFAULT_SERVICES_URL = "/portal/rest/management";
