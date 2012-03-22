6ce85de21ff70c7915426540284ab2f0b669d630
haint <haithanh0809@gmail.com>
2012-03-22 10:57:38 +0700

    Lazy load for sitemap juzu portlet

--- portlet/juzu/sitemap/src/main/webapp/public/javascripts/sitemap.js
+++ portlet/juzu/sitemap/src/main/webapp/public/javascripts/sitemap.js
@@ -6,4 +6,36 @@ $(function() {
 		prerendered : true,
 		persist : "location"
 	});
+	
+	$(".jz").bind("lazyload", function() {
+		$(".expandable").each(function() {
+			var child = $(this).find("ul");
+			if(child.length == 1) {
+				if(!child[0].childElementCount) {
+					$(this).one("click", function() {
+						var loadAction = this.SitemapApplication().loadChild();
+						var param = {};
+						var id = $(this).attr("id");
+						param["id"] = id.substr(0,id.indexOf("-"));
+						var result;
+						var ajaxGet = $.ajax({
+							type: "get",
+							data: param,
+						    url: loadAction,
+						    dataType: "html",
+						    success: function(data) {
+						    	var branches = $(data).appendTo(child[0]);
+								$("#tree").treeview({
+									add: branches
+								});
+								$(".jz").trigger("lazyload");
+						    }
+						});
+						
+					});
+				}
+			}
+		});
+	});
+	$(".jz").trigger("lazyload");
 })
