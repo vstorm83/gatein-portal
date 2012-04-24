f2ef0a7b91b3c0c23b47e85819780b55dcf44e91
haint <haithanh0809@gmail.com>
2012-04-24 10:03:58 +0700

    Juzu page management: auto ajust height

--- portlet/juzu/page-management/src/main/webapp/public/javascripts/pages.js
+++ portlet/juzu/page-management/src/main/webapp/public/javascripts/pages.js
@@ -1,8 +1,26 @@
 $(function() {
-	$('#PageMamangement').submit(function() {
+	$('#PageMamangementForm').submit(function() {
 		$(this).ajaxSubmit({
-			target: '#pageList'
+			target: '#PageList',
+			success: function() {
+				autoAjust();
+			}	
 		});
 		return false;
 	});
+	
+	var autoAjust = function() {
+		var pageList =$("#PageListContainer");
+		
+		pageList.height("auto");
+		var eleHeight = 0; 
+		pageList.children().each(function() {
+			eleHeight += $(this).height();
+		});
+		
+		var height = $(document).height() - pageList.height() - 20;
+		if(pageList.height() > height && eleHeight > height) pageList.height(height);
+	};
+	
+	autoAjust();
 });
