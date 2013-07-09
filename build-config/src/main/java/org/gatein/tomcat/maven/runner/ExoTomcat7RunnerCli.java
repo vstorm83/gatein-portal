ff01f5414f0197899ccfd75e3d58dc81508a671b
phuong_vu <phuong.vu@exoplatform.com>
2013-07-09 11:41:04 +0700

    Override main class of maven tomcat runner

---
+++ build-config/src/main/java/org/gatein/tomcat/maven/runner/ExoTomcat7RunnerCli.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2013 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+  
+package org.gatein.tomcat.maven.runner;
+
+import org.apache.tomcat.maven.runner.Tomcat7RunnerCli;
+
+public class ExoTomcat7RunnerCli extends Tomcat7RunnerCli {
+    
+    public static String JAAS_KEY="java.security.auth.login.config";
+    
+    public static String JAAS_DEFAULT_PATH="../web/src/main/tomcatconf/jaas.conf";
+    
+    public static void main( String[] args ) throws Exception {
+        if (System.getProperty(JAAS_KEY) == null) {
+            System.setProperty(JAAS_KEY, JAAS_DEFAULT_PATH);
+        }
+        Tomcat7RunnerCli.main(args);
+    }
+}
